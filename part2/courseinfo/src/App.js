8b1a0e93ee2518fd0c1e79031ceeb6d67a654229
chrislange89 <chris_lange_89@hotmail.com>
2023-07-05 11:30:02 +1000

    formatting id

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -7,19 +7,19 @@ const App = () => {
       name: 'Half Stack application development',
       parts: [
         {
-          id: 1,
           name: 'Fundamentals of React',
           exercises: 10,
+          id: 1,
         },
         {
-          id: 2,
           name: 'Using props to pass data',
           exercises: 7,
+          id: 2,
         },
         {
-          id: 3,
           name: 'State of a component',
           exercises: 14,
+          id: 3,
         },
       ]
     },

e20baa086736163c50b115ddf84d5b53d8581652
chrislange89 <chris_lange_89@hotmail.com>
2023-07-10 09:36:51 +1000

    initial data loaded from json-server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -16,9 +16,7 @@ const App = () => {
     })
   } 
 
-  useEffect(() => {
-    getInitialPersons();
-  }, [])
+  useEffect(getInitialPersons, []);
 
   const [persons, setPersons] = useState([]); 
 

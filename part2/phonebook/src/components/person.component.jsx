0bc13058ee31fea93c02ac2613c90b4a3f567bed
chrislange89 <chris_lange_89@hotmail.com>
2023-07-12 12:36:00 +1000

    add ability to delete entries

--- part2/phonebook/src/components/person.component.jsx
+++ part2/phonebook/src/components/person.component.jsx
@@ -1,14 +1,19 @@
 import PropTypes from 'prop-types'; 
 
-const Person = ({ name, number }) => {
+const Person = ({ id, name, number, handleDelete }) => {
   return (
-    <p>{name} - {number}</p>
+      <p>{name} - {number}
+        <button id={id} type="button" onClick={handleDelete}>Delete</button>
+      </p>
+    
   )
 }
 
 Person.propTypes = {
+  id: PropTypes.any.isRequired,
   name: PropTypes.string.isRequired,
-  number: PropTypes.string.isRequired
+  number: PropTypes.string.isRequired,
+  handleDelete: PropTypes.func.isRequired
 }
 
 export default Person;

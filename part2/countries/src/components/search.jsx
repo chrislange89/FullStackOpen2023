68b74b720b64258dadca1e9fb7a65d2494dab443
chrislange89 <chris_lange_89@hotmail.com>
2023-07-14 06:53:04 +1000

    formatting search component

--- part2/countries/src/components/search.jsx
+++ part2/countries/src/components/search.jsx
@@ -2,7 +2,12 @@ const Search = ({ search, handleSearch }) => {
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input value={search} onChange={handleSearch} type="text" id="search" />
+      <input 
+        value={search} 
+        onChange={handleSearch} 
+        type="text" 
+        id="search" 
+      />
     </div>
   )
 }

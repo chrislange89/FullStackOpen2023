6138ce50808a3fdafa0bc64602fb9cc2116447aa
chrislange89 <chris_lange_89@hotmail.com>
2023-07-14 09:03:05 +1000

    hide data if too many entries

--- part2/countries/src/components/countries.jsx
+++ part2/countries/src/components/countries.jsx
@@ -12,6 +12,12 @@ export default function Countries({ countries }) {
     );
   }
 
+  if (countries.length > 10) {
+    return (
+      <p>Too many matches, specify another filter</p>
+    );
+  }
+
   return (
     <>
       {countries.map((country) => (

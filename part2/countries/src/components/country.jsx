9e1f2e837a5ebdff41bcf660578456b268eb11e7
chrislange89 <chris_lange_89@hotmail.com>
2023-07-14 09:02:55 +1000

    add ability to expand country

--- part2/countries/src/components/country.jsx
+++ part2/countries/src/components/country.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 function Country({ country }) {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleClick = () => {
+    setExpanded(!expanded);
+  };
+
+  if (!expanded) {
+    return (
+      <p>
+        <h2>{country.name.common}</h2>
+        <button onClick={handleClick} type="button">show</button>
+      </p>
+    );
+  }
+
   return (
-    <p>{country.name.common}</p>
+    <p>
+      <h2>{country.name.common}</h2>
+      <button onClick={handleClick} type="button">hide</button>
+      <div>
+        <p>
+          <strong>Flag: </strong>
+          <span>{country.flag}</span>
+        </p>
+        <p>
+          <strong>Capital City: </strong>
+          {country.capital[0]}
+        </p>
+        <p>
+          population
+          {country.population}
+        </p>
+      </div>
+    </p>
   );
 }
 

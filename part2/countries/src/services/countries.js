b901d31cfeeddc9bcb4fdd64c65a61c4c08006a8
chrislange89 <chris_lange_89@hotmail.com>
2023-07-14 14:05:19 +1000

    linting

--- part2/countries/src/services/countries.js
+++ part2/countries/src/services/countries.js
@@ -1,10 +1,10 @@
 import Axios from 'axios';
 
-const BASEURL = 'https://studies.cs.helsinki.fi/restcountries/api/all'
+const BASEURL = 'https://studies.cs.helsinki.fi/restcountries/api/all';
 
 function getAll() {
   const request = Axios.get(BASEURL);
-  const foundCountries = request.then(response => response.data);
+  const foundCountries = request.then((response) => response.data);
   return foundCountries;
 }
 

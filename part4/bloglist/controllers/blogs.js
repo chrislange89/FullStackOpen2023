8d558f2b2e98d36829a1a2b8bd0d0f919d855b1f
chrislange89 <chris_lange_89@hotmail.com>
2023-07-25 08:36:50 +1000

    add error handling

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,21 +1,51 @@
 const Router = require('express').Router();
 const Blog = require('../models/blog');
+const logger = require('../utils/logger');
 
-Router.get('/', (request, response) => {
+Router.get('/', (request, response, next) => {
   Blog
     .find({})
     .then((blogs) => {
       response.json(blogs);
+    })
+    .catch((error) => {
+      next(error);
     });
 });
 
-Router.post('/', (request, response) => {
+Router.post('/', (request, response, next) => {
   const blog = new Blog(request.body);
   blog
     .save()
-    .then((result) => {
-      response.status(201).json(result);
-    });
+    .then((result) => response.status(201).json(result))
+    .catch((error) => next(error));
+});
+
+Router.delete('/:id', (request, response, next) => {
+  const { id } = request.params;
+  logger.info(request.params);
+  Blog
+    .findOneAndRemove({ _id: id })
+    .then(() => response.status(204).end())
+    .catch((error) => next(error));
+});
+
+Router.put('/:id', (request, response, next) => {
+  const { body } = request;
+  const { id } = request.params;
+
+  const blog = {
+    likes: body.likes,
+    title: body.title,
+    author: body.author,
+    url: body.url,
+  };
+
+  Blog.findByIdAndUpdate(id, blog, { new: true })
+    .then((updatedBlog) => {
+      response.json(updatedBlog);
+    })
+    .catch((error) => next(error));
 });
 
 module.exports = Router;

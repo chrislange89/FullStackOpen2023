c82028d9dae41c8803ea1d93fe2d1ac42905f200
chrislange89 <chris_lange_89@hotmail.com>
2023-07-25 07:32:32 +1000

    reformat app.js

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,16 +1,22 @@
 const express = require('express');
+
+const app = express();
+
+// node packages
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-const blogRouter = require('./controllers/blogs');
-
+// config
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 const middleware = require('./utils/middleware');
 
-const app = express();
+// routers
+const blogRouter = require('./controllers/blogs');
 
 const mongooseConnectionString = config.MONGODB_URI.replace('<password>', config.MONGODB_PASSWORD).replace('<user>', config.MONGODB_USER);
+
+logger.info('Connecting to:', config.MONGODB_URI);
 
 mongoose.connect(mongooseConnectionString)
   .then(() => {
@@ -20,12 +26,12 @@ mongoose.connect(mongooseConnectionString)
     logger.error('error connecting to MongoDB:', error.message);
   });
 
-app.use(middleware.requestLogger);
-app.use('/api/blogs', blogRouter);
-
 app.use(cors());
+app.use(express.static('build'));
 app.use(express.json());
+app.use(middleware.requestLogger);
 
+app.use('/api/blogs', blogRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);

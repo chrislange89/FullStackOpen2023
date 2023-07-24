8666fad9d21d72741f8c1049ea7f2f4b8069f15a
chrislange89 <chris_lange_89@hotmail.com>
2023-07-25 06:39:23 +1000

    create blog schema

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,12 +4,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
+
 
 const Blog = mongoose.model('Blog', blogSchema);
 
